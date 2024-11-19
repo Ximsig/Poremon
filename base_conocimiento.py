d664a9a9f5d4aaab680fbd23d6ea555d4571f964
Jordistt <jordisototripiana@gmail.com>
2024-11-19 10:44:04 +0100

    base

---
+++ base_conocimiento.py
@@ -0,0 +1,43 @@
+
+
+debilidades = {
+    'Acero' : ['Fuego','Lucha', 'Tierra'],
+    'Agua' : ['Planta', 'Electrico'],
+    'Bicho' : ['Fuego', 'Roca', 'Volador'],
+    'Dragon' : ['Dragón', 'Hada', 'Hielo'],
+    'Electrico' : ['Tierra'],
+    'Fantasma' : ['Fantasma', 'Siniestro'],
+    'Fuego' : ['Agua', 'Roca', 'Tierra'],
+    'Hada' : ['Acero', 'Veneno'],
+    'Hielo' : ['Acero', 'Fuego', 'Lucha', 'Roca'],
+    'Lucha' : ['Hada', 'Psíquico', 'Volador'],
+    'Normal' : ['Lucha'],
+    'Planta' : ['Bicho', 'Fuego', 'Hielo', 'Veneno', 'Volador'],
+    'Psiquico' : ['Bicho', 'Fantasma', 'Siniestro'],
+    'Roca' : ['Acero', 'Agua', 'Lucha', 'Planta', 'Tierra'],
+    'Siniestro' : ['Bicho', 'Hada', 'Lucha'],
+    'Tierra' : ['Agua', 'Hielo', 'Planta'],
+    'Veneno' : ['Psíquico', 'Tierra'],
+    'Volador' : ['Eléctrico', 'Hielo', 'Roca']
+}
+
+efectividades = {
+    'Acero': ['Hada', 'Hielo', 'Roca'],
+    'Agua': ['Fuego', 'Roca', 'Tierra'],
+    'Bicho': ['Hada', 'Planta', 'Psíquico'],
+    'Dragon': ['Dragón'],
+    'Eléctrico': ['Agua', 'Volador'],
+    'Fantasma': ['Fantasma', 'Psíquico'],
+    'Fuego': ['Acero', 'Bicho', 'Hielo', 'Planta'],
+    'Hada': ['Dragón', 'Lucha', 'Siniestro'],
+    'Hielo': ['Dragón', 'Planta', 'Tierra', 'Volador'],
+    'Lucha': ['Acero', 'Hielo', 'Normal', 'Roca', 'Siniestro'],
+    'Normal': [],
+    'Planta': ['Agua', 'Roca', 'Tierra'],
+    'Psíquico': ['Lucha', 'Veneno'],
+    'Roca': ['Bicho', 'Fuego', 'Hielo', 'Volador'],
+    'Siniestro': ['Fantasma', 'Psíquico'],
+    'Tierra': ['Eléctrico', 'Fuego', 'Roca', 'Veneno', 'Acero'],
+    'Veneno': ['Hada', 'Planta'],
+    'Volador': ['Bicho', 'Lucha', 'Planta']
+}
